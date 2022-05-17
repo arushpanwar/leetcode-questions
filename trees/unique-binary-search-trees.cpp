3205effa9f12333e7bdc1f80c2b9dc8b14d17615
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-05-17 21:33:28 +0530

    using bottom up dp

--- trees/unique-binary-search-trees.cpp
+++ trees/unique-binary-search-trees.cpp
@@ -31,3 +31,17 @@ int totalBst(int n,vector<int>& dp){
         vector<int> dp(n+1,0);
         return totalBst(n,dp);
     }
+
+
+//Bottom Up DP
+int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
