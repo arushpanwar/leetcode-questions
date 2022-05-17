b8d85e72ad9ba78f42d7f926e678d7b3ac135c7f
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-05-17 09:12:48 +0530

    assuming no duplicate values

---
+++ daily/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool dfs(TreeNode* root,int value){
+        if(!root)return false;
+        if(root->val==value)return true;
+        return dfs(root->left,value)||dfs(root->right,value);
+    }
+    TreeNode* getTarget(TreeNode* root,int value){
+        if(root->val==value)return root;
+        if(dfs(root->left,value))return getTarget(root->left,value);
+        return getTarget(root->right,value);
+    }
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!original)return target;
+        int value=target->val;
+        return getTarget(cloned,value);
+    }
+};
