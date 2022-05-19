96cd9e09b40cfc20d8ecd4065bd54f20dc6775e8
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-05-20 00:18:45 +0530

    another soln with o(n)

--- dp/counting-bits.cpp
+++ dp/counting-bits.cpp
@@ -6,3 +6,17 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i/2]+(i&1);
+        }
+        return ans;
+    }
+};
