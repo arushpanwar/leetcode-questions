05150dbb4c650528c6c46f8951cf556fe11dcdc2
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-05-20 10:00:55 +0530

    oms iterative

--- daily/unique-paths-ii.cpp
+++ daily/unique-paths-ii.cpp
@@ -19,3 +19,23 @@ public:
         return solve(obstacleGrid,dp,0,0);
     }
 };
+
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<long long int>> dp(n,vector<long long int> (m,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(grid[i][j]==0){
+                    if(i==n-1&&j==m-1)dp[i][j]=1;
+                    else if(i==n-1)dp[i][j]=dp[i][j+1];
+                    else if(j==m-1)dp[i][j]=dp[i+1][j];
+                    else dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
