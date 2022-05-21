9c12fdb633c6bfa92844faf4a96c8a5299a86895
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-05-21 17:41:03 +0530

    done with bottom up dp 1d

--- dp/coin-change.cpp
+++ dp/coin-change.cpp
@@ -17,3 +17,22 @@ public:
         return solve(coins,amount,dp);
     }
 };
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<int> dp(amount+1,INT_MAX-1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                if(coins[j]<=i&&dp[i-coins[j]]!=-1){
+                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+            if(dp[i]==INT_MAX-1)dp[i]=-1;
+        }
+        return dp[amount];
+    }
+};
