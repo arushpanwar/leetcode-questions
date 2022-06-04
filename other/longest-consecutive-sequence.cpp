e83937a3d7808c26fca24d70ac7edbff72d667b4
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-06-04 22:20:22 +0530

    added other solutions of o(n)

--- other/longest-consecutive-sequence.cpp
+++ other/longest-consecutive-sequence.cpp
@@ -19,4 +19,33 @@ public:
         }
         return mx;
     }
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            if(m.count(nums[i]-1))m[nums[i]]=m[nums[i]-1]+1;
+            else m[nums[i]]=1;
+        }
+        int ans=INT_MIN;
+        for(auto x:m)ans=max(ans,x.second);
+        return ans;
+    }
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        unordered_set <int> s;
+        int ans=INT_MIN;
+        for(auto x:nums)s.insert(x);
+        for(int i=0;i<nums.size();i++){
+            if(s.count(nums[i]-1)==0){
+                int x=nums[i],cnt=0;
+                while(s.count(x)){
+                    cnt++;
+                    x++;
+                }
+                ans=max(ans,cnt);
+            }
+        }
+        return ans;
+    }
 };
