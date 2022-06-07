055a9c7c881044f8af248f5d99f19d7291a6511b
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-06-07 09:59:42 +0530

    both recursive and iterative solution

--- linked list/reverse-nodes-in-k-group.cpp
+++ linked list/reverse-nodes-in-k-group.cpp
@@ -33,4 +33,28 @@ public:
         }
         return prev;
     }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head||k==1)return head;
+        ListNode* dummy=new ListNode();
+        dummy->next=head;
+        ListNode *prev=dummy,*curr=dummy,*next=dummy;
+        int count=0;
+        while(curr->next!=NULL){
+            curr=curr->next;
+            count++;
+        }
+        while(count>=k){
+            curr=prev->next;
+            next=curr->next;
+            for(int i=1;i<k;i++){
+                curr->next=next->next;
+                next->next=prev->next;
+                prev->next=next;
+                next=curr->next;
+            }
+            prev=curr;
+            count-=k;
+        }
+        return dummy->next;
+    }
 };
