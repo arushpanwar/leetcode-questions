18a9f308f8309c77ea541bf76995cde17e8a279b
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-06-07 23:26:52 +0530

    very easy two pointer approach

---
+++ arrays/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size(),ptr=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                ptr++;
+                nums[ptr]=nums[i];
+            }
+        }
+        return ptr+1;
+    }
+};
