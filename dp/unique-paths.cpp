267fd18b3eaee21c5c8e9e7cb7334d625a2da522
Arush Panwar <35171932+arushpanwar@users.noreply.github.com>
2022-06-18 23:07:15 +0530

    most optimal

--- dp/unique-paths.cpp
+++ dp/unique-paths.cpp
@@ -12,3 +12,15 @@ public:
         return dp[1][1];
     }
 };
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n,1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[j]+=dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+};
